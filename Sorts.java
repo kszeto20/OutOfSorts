f2514e5c87d5c8ca55577694a264934b54e38bca
Kirsten Szeto <kszeto20@stuy.edu>
2020-12-18 00:20:18 -0500

    Now it sorts from lowest to highest; need to figure out how to avoid out of bound error for the last data point

--- Sorts.java
+++ Sorts.java
@@ -1,14 +1,21 @@
 import java.util.Arrays;
 public class Sorts{
   public static void bubbleSort(int[] data) {
-
     for (int i = 0; i < data.length; i++) {
-      for (int j = 1; j < data.length - 1; j++) {
-        if (data[i] > data [j]) {
-          int moving = data[j];
-          data[j] = data[i];
+      int comp = data[i];
+      for (int j = i; j < data.length - 1; j++) {
+        System.out.println(Arrays.toString(data));
+        if (comp > data [j + 1]) {
+          int next = data[j + 1];
+          data[j + 1] = data[i];
+          data[i] = next;
+          /*
+          int moving = data[j + 1];
+          data[j + 1] = data[i];
+          System.out.println(Arrays.toString(data));
           data[i] = moving;
           System.out.println(Arrays.toString(data));
+          */
         }
       }
     }
